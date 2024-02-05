d409a2ae32a99b2b8f89552ce70e4080a76e35f3
ohadn <ohad111@gmail.com>
2024-02-05 01:35:46 +0100

    Add new SelectElement component in the UI package
    A new SelectElement component is added to the UI package. This component is a dropdown select field designed to handle an array of options and user selections. Its implementation includes handleSelectChange method for selection updates, and functionality to render given options.

---
+++ packages/ui/src/SelectElement.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import React from "react";
+import {FormControl, InputLabel, MenuItem, Select} from "@mui/material";
+
+export type Option = {
+  value: string
+  label: string
+}
+
+type SelectElementProps = {
+  options: Option[]
+  onSelect: (selected: Option[]) => void
+}
+
+const SelectElement: React.FC<SelectElementProps> = ({options, onSelect, ...props}) => {
+  const [selectedItems, setSelectedItems] = React.useState<string[]>([])
+
+  const handleSelectChange = (event: any) => {
+
+    setSelectedItems(event.target.value as string[])
+
+    const selectedOptions: Option[] = options
+      .filter(option => (event.target.value as string[]).includes(option.value))
+
+    onSelect(selectedOptions)
+  }
+
+  return <FormControl fullWidth>
+    <InputLabel>Timezones</InputLabel>
+    <Select
+      multiple
+      label="Timezones"
+      value={selectedItems}
+      onChange={handleSelectChange}
+    >
+      {options.map(item => <MenuItem key={item.value} value={item.value}>{item.label}</MenuItem>)}
+    </Select>
+  </FormControl>
+}
+
+export default SelectElement
