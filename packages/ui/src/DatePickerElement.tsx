56778ddc3b38842aa8197f106a86c29f5fc8da64
ohadn <ohad111@gmail.com>
2024-02-05 01:35:54 +0100

    Add DatePickerElement component in UI package
    A new DatePickerElement component is added to the UI package. It leverages the DatePicker from @mui/x-date-pickers package to handle and select dates more conveniently. This component also implements default value settings and responsive styling for full-width display.

---
+++ packages/ui/src/DatePickerElement.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import {DatePicker} from "@mui/x-date-pickers";
+
+import React from "react";
+import dayjs, {Dayjs} from "dayjs";
+
+
+type DatePickerProps = {
+  range?: number
+  onSelect: (selected: Dayjs | null) => void
+}
+
+const DatePickerElement: React.FC<DatePickerProps> = ({range, onSelect, ...props}) => {
+
+  return <DatePicker
+    label='Date'
+    defaultValue={dayjs()}
+    onChange={onSelect}
+    sx={{width: '100%'}}
+  />
+
+}
+
+export default DatePickerElement
