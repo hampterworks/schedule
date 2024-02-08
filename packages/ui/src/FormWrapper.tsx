443df5edaab6512d9ef35f223488368f3bce037c
ohadn <ohad111@gmail.com>
2024-02-08 17:56:41 +0100

    Refactor FormWrapper for enhanced editing
    The FormWrapper component has been updated to allow users to directly add, remove, and edit rows in the form input. This enhancement includes the addition of state management for row editing data and display status, as well as the introduction of new interaction buttons using Material UI icons. Styling has been adjusted accordingly to fit these changes in the component layout.

--- packages/ui/src/FormWrapper.tsx
+++ packages/ui/src/FormWrapper.tsx
@@ -2,10 +2,46 @@
 
 import dayjs, {Dayjs} from "dayjs";
 import {Controller, SubmitHandler, useFieldArray, useForm} from "react-hook-form";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {TimePicker} from "@mui/x-date-pickers";
 import InputElement from "./InputElement";
-import {Button} from "@mui/material";
+import {Button, IconButton} from "@mui/material";
+import AddCircleRoundedIcon from '@mui/icons-material/AddCircleRounded';
+import RemoveCircleRoundedIcon from '@mui/icons-material/RemoveCircleRounded';
+import CheckCircleRoundedIcon from '@mui/icons-material/CheckCircleRounded';
+import CancelRoundedIcon from '@mui/icons-material/CancelRounded';
+import DatePickerElement from "./DatePickerElement";
+import styled from "@emotion/styled";
+import React from "react";
+
+const FormElement = styled.form`
+    li {
+        display: flex;
+        align-items: center;
+        gap: 8px;
+        margin-bottom: 16px;
+        padding-bottom: 8px;
+        border-bottom: 1px solid #d5d5d5;
+
+        > div:first-of-type {
+            flex-basis: 200px;
+            white-space: normal;
+            overflow-wrap: break-word;
+        }
+
+        &:last-of-type {
+            border-bottom: none;
+        }
+    }
+`
+
+const EditRowElement = styled.li`
+    background: #ececec;
+    padding: 8px 16px;
+    border-radius: 4px;
+`
+
+const updatedDayjs = (date: Dayjs, time: string): Dayjs => dayjs(`${date.format('MM-DD-YY')} ${time}`, 'MM-DD-YY HH:mm')
 
 export type DateItem = {
   date: string
@@ -26,6 +62,8 @@ type FormWrapperProps = {
 
 
 const FormWrapper: React.FC<FormWrapperProps> = ({dateRange, onTimeSelect, ...props}) => {
+  const [editingRowDisplayData, setEditingRowDisplayData] = useState<string[]>([])
+  const [editingRowData, setEditingRowData] = useState<Record<string, Partial<DateItem>>>()
   const {handleSubmit, control, formState: {errors}} = useForm<Inputs>();
   const {fields, append, remove} = useFieldArray({
     control,
@@ -39,13 +77,13 @@ const FormWrapper: React.FC<FormWrapperProps> = ({dateRange, onTimeSelect, ...pr
 
   const onSubmit: SubmitHandler<Inputs> = (data) => {
     const formattedDate = data.scheduleRow.map(item => {
-      const updatedDayjs = dayjs(`${item.dayjs.format('MM-DD-YY')} ${item.time}`, 'MM-DD-YY HH:mm')
-      const epochTime = updatedDayjs.unix()
+      const updatedDateTime = updatedDayjs(item.dayjs, item.time)
+      const epochTime = updatedDateTime.unix()
       const epochDay = dayjs(item.dayjs.format('MM-DD-YY')).unix()
 
       return {
         ...item,
-        dayjs: updatedDayjs,
+        dayjs: updatedDateTime,
         ...(isNaN(epochTime)
           ? {discordTime: `<t:${epochDay}:F> (<t:${epochDay}:R>) ${item.description}`}
           : {discordTime: `<t:${epochTime}:F> (<t:${epochTime}:R>) ${item.description}`})
@@ -54,49 +92,137 @@ const FormWrapper: React.FC<FormWrapperProps> = ({dateRange, onTimeSelect, ...pr
     onTimeSelect(formattedDate)
   }
 
-  return <form onSubmit={handleSubmit(onSubmit)} {...props}>
+  return <FormElement onSubmit={handleSubmit(onSubmit)} {...props}>
     {
       dateRange !== null &&
       <>
         <ul>
           {fields.map((item, index) => (
-            <li key={item.id}>
-              <div>
-                {item.date}
-              </div>
-              <Controller
-                name={`scheduleRow.${index}.time`}
-                control={control}
-                defaultValue=""
-                render={({field}) =>
+            <React.Fragment key={item.id}>
+              <li >
+                <div>
+                  {item.date}
+                </div>
+                <Controller
+                  name={`scheduleRow.${index}.time`}
+                  control={control}
+                  defaultValue=""
+                  render={({field}) =>
+                    <TimePicker
+                      label='Starting time'
+                      value={field.value ? dayjs(field.value, "HH:mm") : null}
+                      onChange={(date) => {
+                        field.onChange(date ? date.format("HH:mm") : "")
+                      }}
+                    />
+                  }
+                />
+                <Controller
+                  name={`scheduleRow.${index}.description`}
+                  control={control}
+                  defaultValue=""
+                  render={({field}) => (
+                    <InputElement
+                      type="text"
+                      label="Enter description"
+                      onSelect={(val) => field.onChange(val)}
+                    />
+                  )}
+                />
+                <div>
+                  <IconButton
+                    onClick={() => remove(index)}>
+                    <RemoveCircleRoundedIcon color="primary" />
+                  </IconButton>
+                  <IconButton
+                    onClick={() => {
+                    setEditingRowDisplayData([...editingRowDisplayData, item.id])
+                  }}>
+                    <AddCircleRoundedIcon color="primary" />
+                  </IconButton>
+                </div>
+              </li>
+              {
+                editingRowDisplayData.includes(item.id) &&
+                <EditRowElement>
+                  <DatePickerElement onSelect={(selected) => {
+                    if (selected) {
+                      setEditingRowData(prevState => ({
+                        ...prevState,
+                        [item.id]: {
+                          ...(prevState ? prevState[item.id] : {}),
+                          date: selected.format('dddd, MM-DD-YY'),
+                          dayjs: selected
+                        }
+                      }))
+                    }
+                  }}/>
                   <TimePicker
                     label='Starting time'
-                    value={field.value ? dayjs(field.value, "HH:mm") : null}
-                    onChange={(date) => {
-                      field.onChange(date ? date.format("HH:mm") : "")
-                    }}
-                  />
-                }
-              />
-              <Controller
-                name={`scheduleRow.${index}.description`}
-                control={control}
-                defaultValue=""
-                render={({field}) => (
+                    onChange={(date: Dayjs | null) => {
+                      if (date) {
+                        setEditingRowData(prevState => ({
+                          ...prevState,
+                          [item.id]: {
+                            ...(prevState ? prevState[item.id] : {}),
+                            time: date ? date.format("HH:mm") : ""
+                          }
+                        }))
+                      }
+                    }}/>
                   <InputElement
                     type="text"
                     label="Enter description"
-                    onSelect={(val) => field.onChange(val)}
+                    onSelect={(selected) => {
+                      if (selected) {
+                        setEditingRowData(prevState => ({
+                          ...prevState,
+                          [item.id]: {
+                            ...(prevState ? prevState[item.id] : {}),
+                            description: selected
+                          }
+                        }))
+                      }
+                    }}
                   />
-                )}
-              />
-            </li>
+                  <div>
+                    <IconButton onClick={() => setEditingRowDisplayData(editingRowDisplayData.filter(row => row !== item.id))}>
+                      <CancelRoundedIcon color="error"
+                      />
+                    </IconButton>
+                    {
+                      editingRowData !== undefined &&
+                      <IconButton onClick={() => {
+                        if (editingRowData[item.id] !== undefined) {
+                          const editedDayjs = editingRowData[item.id]?.dayjs ?? dayjs()
+                          const date = editingRowData[item.id]?.date ?? dayjs().format('dddd, MM-DD-YY')
+                          const time = editingRowData[item.id]?.time ?? ''
+                          const description = editingRowData[item.id]?.description ?? ''
+
+                          append({date: date, time: time, description: description, dayjs: editedDayjs})
+
+                          setEditingRowData(() => {
+                            let newData = {...editingRowData}
+                            delete newData[item.id]
+                            return newData
+                          })
+                          setEditingRowDisplayData(editingRowDisplayData.filter(value => value !== item.id))
+                        }
+                      }}>
+                        <CheckCircleRoundedIcon color="success"/>
+                      </IconButton>
+                    }
+
+                  </div>
+                </EditRowElement>
+              }
+            </React.Fragment>
           ))}
         </ul>
         <Button sx={{width: '100%'}} type="submit" variant="contained">Submit</Button>
       </>
     }
-  </form>
+  </FormElement>
 }
 
 export default FormWrapper
