7338a07736410eff58d45fc97de214fa715b167d
ohadn <ohad111@gmail.com>
2024-05-15 00:51:57 +0200

    Update ButtonWrapper for dynamic usage
    Enhanced ButtonWrapper in ButtonWrapper.tsx to become more reusable by replacing the static removeTemplate and addTemplateAfter methods with dynamic removeItemFunction and addItemFunction. Also, added conditional rendering for remove and add buttons with the hideAdd and hideRemove properties. Styled with flexbox for better component layout.

--- packages/ui/src/ButtonWrapper.tsx
+++ packages/ui/src/ButtonWrapper.tsx
@@ -1,26 +1,50 @@
 "use client"
 import CancelRoundedIcon from '@mui/icons-material/CancelRounded';
 import AddCircleRoundedIcon from '@mui/icons-material/AddCircleRounded';
-import { IconButton } from "@mui/material";
-import {Template} from "web/state/schedule";
+import {IconButton} from "@mui/material";
 import React from "react";
+import styled from "@emotion/styled";
 
-type ButtonWrapperProps = {
-  removeTemplate: (index: number) => void
-  addTemplateAfter: (index: number, template: Template) => void
-  template: Template
+const ButtonContainer = styled.div`
+    display: flex;
+    flex-basis: 40px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+type ButtonWrapperProps<T> = {
+  removeItemFunction: (index: number) => void
+  addItemFunction: (index: number, template: T) => void
+  template: T
   index: number
+  hideAdd?: boolean
+  hideRemove?: boolean
 }
 
-const ButtonWrapper: React.FC<ButtonWrapperProps> = ({index, template, removeTemplate, addTemplateAfter}) => {
-  return <div>
-    <IconButton onClick={() => removeTemplate(index)}>
-      <CancelRoundedIcon/>
-    </IconButton>
-    <IconButton onClick={() => addTemplateAfter(index, {date: template.date})}>
-      <AddCircleRoundedIcon/>
-    </IconButton>
-  </div>
+function ButtonWrapper<T>
+({
+   index,
+   template,
+   removeItemFunction,
+   addItemFunction,
+   hideAdd,
+   hideRemove
+ }: ButtonWrapperProps<T>) {
+  return <ButtonContainer>
+    {
+      hideRemove !== true &&
+      <IconButton onClick={() => removeItemFunction(index)}>
+        <CancelRoundedIcon/>
+      </IconButton>
+    }
+    {
+      hideAdd !== true &&
+      <IconButton onClick={() => addItemFunction(index, template)}>
+        <AddCircleRoundedIcon/>
+      </IconButton>
+    }
+  </ButtonContainer>
 }
 
 export default ButtonWrapper
