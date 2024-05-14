144bdf1af543a8ff5e1b695321677e32ec65ba82
ohadn <ohad111@gmail.com>
2024-05-15 00:53:22 +0200

    Update DesignDisplay and HeaderDesigner to handle socials
    Code components DesignDisplay and HeaderDesigner are updated to handle and display social media tags. Social media icons for Twitch, Twitter, and YouTube have been added. Colour, layout, and component state adjustments were also made.

--- packages/ui/src/DesignDisplay.tsx
+++ packages/ui/src/DesignDisplay.tsx
@@ -1,13 +1,12 @@
 "use client"
 
 import React, {useRef, useState} from "react";
-import type {DesignTemplate, Template} from "web/state/schedule";
+import {SocialNetworks, Socials, Template} from "web/state/schedule";
 import {DateTime} from "luxon";
 import styled from "@emotion/styled";
 import html2canvas from 'html2canvas';
 import ButtonElement from "./ButtonElement";
 import {macFontList, winFontList} from "web/data/fonts";
-import {MenuItem, Select} from "@mui/material";
 import HeaderDesigner from "./HeaderDesigner";
 
 const DesignResults = styled.div<{ background: string }>`
@@ -18,7 +17,7 @@ const DesignResults = styled.div<{ background: string }>`
     grid-template-rows: 10% 80% 10%;
     row-gap: 16px;
 
-    background: #fdddd8;
+    background: #ffffff;
     background-image: ${props => `url(${props.background})`};
     background-size: contain;
     background-repeat: no-repeat;
@@ -28,6 +27,7 @@ const DesignResults = styled.div<{ background: string }>`
 const DesignHeader = styled.div`
     grid-column: 3;
     grid-row: 1;
+    color: #131313;
 
     h1 {
         font-size: 24px;
@@ -61,9 +61,9 @@ const DayName = styled.div`
     justify-content: center;
     align-items: center;
     font-size: 24px;
-    background: #feb6be;
+    background: #82a7ec;
     border-radius: 4px 0 0 4px;
-    
+
     span:last-of-type {
         font-size: 18px;
     }
@@ -157,149 +157,160 @@ const getAvailableFonts = (): string[] => {
 
 const getWeekDurationString = (templates: Template[]): string => {
   const firstDay = (typeof templates[0]?.date === 'string')
-      ? DateTime.fromISO(templates[0].date).toFormat('d/L')
-      : templates[0]?.date.toFormat('d/L')
+    ? DateTime.fromISO(templates[0].date).toFormat('d/L')
+    : templates[0]?.date.toFormat('d/L')
 
-  const lastDay = (typeof templates[templates.length -1]?.date === 'string' )
-    ? DateTime.fromISO(templates[templates.length -1]?.date as string).toFormat('d/L')
-    : (templates[templates.length -1]?.date as DateTime).toFormat('d/L')
+  const lastDay = (typeof templates[templates.length - 1]?.date === 'string')
+    ? DateTime.fromISO(templates[templates.length - 1]?.date as string).toFormat('d/L')
+    : (templates[templates.length - 1]?.date as DateTime).toFormat('d/L')
 
   return `Week of ${firstDay} - ${lastDay}`
+}
+
+const getSocialNetworkIcon = (network: SocialNetworks): React.ReactNode => {
+  switch (network) {
+    case 'twitch':
+      return <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                  className="bi bi-twitch"
+                  viewBox="0 0 16 16">
+        <path
+          d="M3.857 0 1 2.857v10.286h3.429V16l2.857-2.857H9.57L14.714 8V0zm9.714 7.429-2.285 2.285H9l-2 2v-2H4.429V1.143h9.142z"/>
+        <path d="M11.857 3.143h-1.143V6.57h1.143zm-3.143 0H7.571V6.57h1.143z"/>
+      </svg>
+    case "twitter":
+      return <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                  className="bi bi-twitter"
+                  viewBox="0 0 16 16">
+        <path
+          d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334q.002-.211-.006-.422A6.7 6.7 0 0 0 16 3.542a6.7 6.7 0 0 1-1.889.518 3.3 3.3 0 0 0 1.447-1.817 6.5 6.5 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.32 9.32 0 0 1-6.767-3.429 3.29 3.29 0 0 0 1.018 4.382A3.3 3.3 0 0 1 .64 6.575v.045a3.29 3.29 0 0 0 2.632 3.218 3.2 3.2 0 0 1-.865.115 3 3 0 0 1-.614-.057 3.28 3.28 0 0 0 3.067 2.277A6.6 6.6 0 0 1 .78 13.58a6 6 0 0 1-.78-.045A9.34 9.34 0 0 0 5.026 15"/>
+      </svg>
+    case "youtube":
+      return <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                  className="bi bi-youtube"
+                  viewBox="0 0 16 16">
+        <path
+          d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.01 2.01 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.01 2.01 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31 31 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.01 2.01 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A100 100 0 0 1 7.858 2zM6.4 5.209v4.818l4.157-2.408z"/>
+      </svg>
+    default:
+      <></>
+  }
 }
 
 type DesignDisplayProps = {
   templates: Template[]
   timeZones: string[]
-  designTemplate: DesignTemplate
+  mainHeader: string
   setMainHeader: (newHeader: string) => void
+  socials: Socials[]
+  addSocials: (index: number, socials: Socials) => void,
+  removeSocials: (index: number) => void,
 }
 
-const DesignDisplay: React.FC<DesignDisplayProps> = ({timeZones, templates, designTemplate, setMainHeader}) => {
-  const divRef = useRef<HTMLDivElement>(null)
-  const [backgroundImage, setBackgroundImage] = useState('')
-  const [screenshotDataUrl, setScreenshotDataUrl] = useState<string | null>(null)
-  const localTimeZone = DateTime.local().offset / 60
+const DesignDisplay: React.FC<DesignDisplayProps> =
+  ({
+     timeZones,
+     templates,
+     mainHeader,
+     setMainHeader,
+     socials,
+     addSocials,
+     removeSocials
+   }) => {
+    const divRef = useRef<HTMLDivElement>(null)
+    const [backgroundImage, setBackgroundImage] = useState('')
+    const [screenshotDataUrl, setScreenshotDataUrl] = useState<string | null>(null)
+    const localTimeZone = DateTime.local().offset / 60
 
-  const fonts = getAvailableFonts()
+    const fonts = getAvailableFonts()
 
-  const handleUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files && event.target.files[0]
-    if (file) {
-      const reader = new FileReader()
-      reader.onloadend = () => {
-        if (reader.result !== null)
-          setBackgroundImage(reader.result.toString())
-      }
-      reader.readAsDataURL(file)
-    }
-  }
-
-  const takeScreenshot = () => {
-    if (divRef.current) {
-      html2canvas(divRef.current).then(canvas =>
-        setScreenshotDataUrl(canvas.toDataURL('image/png')))
-    }
-  }
-  return <div>
-    <HeaderDesigner designTemplate={designTemplate} setMainHeader={setMainHeader}/>
-    <DesignResults background={backgroundImage} ref={divRef}>
-      <DesignHeader>
-        <h1>{designTemplate.mainHeader}</h1>
-        <div>{getWeekDurationString(templates)}</div>
-      </DesignHeader>
-      <DateWrapper>
-        {
-          templates.map((template, index) => {
-            const date = (typeof template.date === 'string')
-              ? DateTime.fromISO(template.date)
-              : template.date
-
-            const day = date.toFormat('ccc')
-            const dayDate = date.toFormat('d/L')
-            const time = formatTimeWithOptionalMinutes(template.time)
-
-            const timeZoneList = template.time !== undefined
-              ? formatTimeZones(date, template.time, timeZones)
-              : []
-
-            const localTime = time
-              ? `${time} GMT${localTimeZone >= 0 ? '+' : ''}${localTimeZone}`
-              : undefined
-
-            return <DateItem key={day + index}>
-              <DayName>
-                <span>{day}</span>
-                <span>{dayDate}</span>
-              </DayName>
-              <DayDetailsWrapper>
-                <DayDescription>{template.description}</DayDescription>
-                {
-                  localTime !== undefined &&
-                  <TimesWrapper>{`${localTime} ${timeZoneList.length !== 0 ? '/ ' : ''}${timeZoneList.join(" / ")}`}</TimesWrapper>
-                }
-              </DayDetailsWrapper>
-            </DateItem>
-          })
+    const handleUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+      const file = event.target.files && event.target.files[0]
+      if (file) {
+        const reader = new FileReader()
+        reader.onloadend = () => {
+          if (reader.result !== null)
+            setBackgroundImage(reader.result.toString())
         }
-      </DateWrapper>
-      <SocialsWrapper>
-        <li>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-twitch"
-               viewBox="0 0 16 16">
-            <path
-              d="M3.857 0 1 2.857v10.286h3.429V16l2.857-2.857H9.57L14.714 8V0zm9.714 7.429-2.285 2.285H9l-2 2v-2H4.429V1.143h9.142z"/>
-            <path d="M11.857 3.143h-1.143V6.57h1.143zm-3.143 0H7.571V6.57h1.143z"/>
-          </svg>
-          <span>Twitch name</span>
-        </li>
-        <li>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-youtube"
-               viewBox="0 0 16 16">
-            <path
-              d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.01 2.01 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.01 2.01 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31 31 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.01 2.01 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A100 100 0 0 1 7.858 2zM6.4 5.209v4.818l4.157-2.408z"/>
-          </svg>
-          <span>Youtube name</span>
-        </li>
-        <li>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-twitter"
-               viewBox="0 0 16 16">
-            <path
-              d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334q.002-.211-.006-.422A6.7 6.7 0 0 0 16 3.542a6.7 6.7 0 0 1-1.889.518 3.3 3.3 0 0 0 1.447-1.817 6.5 6.5 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.32 9.32 0 0 1-6.767-3.429 3.29 3.29 0 0 0 1.018 4.382A3.3 3.3 0 0 1 .64 6.575v.045a3.29 3.29 0 0 0 2.632 3.218 3.2 3.2 0 0 1-.865.115 3 3 0 0 1-.614-.057 3.28 3.28 0 0 0 3.067 2.277A6.6 6.6 0 0 1 .78 13.58a6 6 0 0 1-.78-.045A9.34 9.34 0 0 0 5.026 15"/>
-          </svg>
-          <span>Twitter name</span>
-        </li>
-      </SocialsWrapper>
-    </DesignResults>
-
-    <input
-      type='file'
-      accept='image/*'
-      onChange={handleUpload}
-    />
-    {/*<Select*/}
-    {/*  labelId="demo-simple-select-label"*/}
-    {/*  id="demo-simple-select"*/}
-    {/*  label="Age"*/}
-    {/*  onChange={(event) => {*/}
-    {/*    console.log(event)*/}
-    {/*  }}*/}
-    {/*>*/}
-    {/*  {*/}
-    {/*    fonts.map(font =>*/}
-    {/*      font !== undefined*/}
-    {/*        ? <MenuItem key={font} value={font}>{font}</MenuItem>*/}
-    {/*        : <MenuItem key={font} value={font}>''</MenuItem>*/}
-    {/*    )*/}
-    {/*  }*/}
-    {/*</Select>*/}
-    <ButtonElement onClick={takeScreenshot}>Take screenshot</ButtonElement>
-    {
-      screenshotDataUrl && <a href={screenshotDataUrl} download="screenshot.png">
-        <ButtonElement>Download screenshot</ButtonElement>
-      </a>
+        reader.readAsDataURL(file)
+      }
     }
-  </div>
-}
+
+    const takeScreenshot = () => {
+      if (divRef.current) {
+        html2canvas(divRef.current).then(canvas =>
+          setScreenshotDataUrl(canvas.toDataURL('image/png')))
+      }
+    }
+    return <div>
+      <HeaderDesigner socials={socials} addSocials={addSocials} removeSocials={removeSocials} mainHeader={mainHeader}
+                      setMainHeader={setMainHeader}/>
+      <DesignResults background={backgroundImage} ref={divRef}>
+        <DesignHeader>
+          <h1>{mainHeader}</h1>
+          <div>{getWeekDurationString(templates)}</div>
+        </DesignHeader>
+        <DateWrapper>
+          {
+            templates.map((template, index) => {
+              const date = (typeof template.date === 'string')
+                ? DateTime.fromISO(template.date)
+                : template.date
+
+              const day = date.toFormat('ccc')
+              const dayDate = date.toFormat('d/L')
+              const time = formatTimeWithOptionalMinutes(template.time)
+
+              const timeZoneList = template.time !== undefined
+                ? formatTimeZones(date, template.time, timeZones)
+                : []
+
+              const localTime = time
+                ? `${time} GMT${localTimeZone >= 0 ? '+' : ''}${localTimeZone}`
+                : undefined
+
+              return <DateItem key={day + index}>
+                <DayName>
+                  <span>{day}</span>
+                  <span>{dayDate}</span>
+                </DayName>
+                <DayDetailsWrapper>
+                  <DayDescription>{template.description}</DayDescription>
+                  {
+                    localTime !== undefined &&
+                    <TimesWrapper>{`${localTime} ${timeZoneList.length !== 0 ? '/ ' : ''}${timeZoneList.join(" / ")}`}</TimesWrapper>
+                  }
+                </DayDetailsWrapper>
+              </DateItem>
+            })
+          }
+        </DateWrapper>
+        <SocialsWrapper>
+          {
+            socials.map((item, index) => {
+              if (item.network !== 'none' && item.tag !== undefined) {
+                return <li key={item.network + index}>
+                  {getSocialNetworkIcon(item.network)}
+                  <span>{item.tag}</span>
+                </li>
+              }
+            })
+          }
+        </SocialsWrapper>
+      </DesignResults>
+
+      <input
+        type='file'
+        accept='image/*'
+        onChange={handleUpload}
+      />
+      <ButtonElement onClick={takeScreenshot}>Take screenshot</ButtonElement>
+      {
+        screenshotDataUrl && <a href={screenshotDataUrl} download="screenshot.png">
+          <ButtonElement>Download screenshot</ButtonElement>
+        </a>
+      }
+    </div>
+  }
 
 export default DesignDisplay
 
--- packages/ui/src/HeaderDesigner.tsx
+++ packages/ui/src/HeaderDesigner.tsx
@@ -3,23 +3,91 @@
 
 import React from "react";
 import InputElement from "./InputElement";
-import type {DesignTemplate} from "web/state/schedule";
+import {SocialNetworks, Socials} from "web/state/schedule";
+import {Button, MenuItem, Select, TextField} from "@mui/material";
+import styled from "@emotion/styled";
+import ButtonWrapper from "./ButtonWrapper";
+
+
+const ControllersWrapper = styled.section`
+  padding: 16px;
+`
+const SocialsWrapper = styled.div`
+  margin-top: 32px;
+`
+const SocialsToolbar = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    gap: 16px;
+`
 
 type HeaderDesignerProps = {
-  designTemplate: DesignTemplate
+  mainHeader: string
+  socials: Socials[]
   setMainHeader: (newHeader: string) => void
+  addSocials: (index: number, socials: Socials) => void,
+  removeSocials: (index: number) => void,
 } & React.ComponentPropsWithoutRef<'section'>
 
-const HeaderDesigner: React.FC<HeaderDesignerProps> = ({designTemplate, setMainHeader, ...props}) => {
-  return <section {...props}>
-    <InputElement
-      label='Total days:'
-      type='text'
-      value={designTemplate.mainHeader}
-      onInput={inputText =>
-        setMainHeader(inputText?? '')}
-    />
-  </section>
-}
+const HeaderDesigner: React.FC<HeaderDesignerProps> =
+  ({
+     mainHeader,
+     setMainHeader,
+     socials,
+     addSocials,
+     removeSocials,
+     ...props
+   }) => {
+
+    return <ControllersWrapper {...props}>
+      <InputElement
+        label='Total days:'
+        type='text'
+        value={mainHeader}
+        onInput={inputText =>
+          setMainHeader(inputText ?? '')}
+      />
+      <SocialsWrapper>
+        {
+          socials.map((item, index) => <SocialsToolbar key={item.network + index}>
+            <TextField
+              select
+              label='Select Network'
+              value={socials[index]?.network}
+              onChange={(event) => {
+                addSocials((index), {...socials[index], network: event.target.value as SocialNetworks})
+              }}
+              style={{flexBasis: '200px'}}
+            >
+              <MenuItem value='none'>None</MenuItem>
+              <MenuItem value='twitch'>Twitch</MenuItem>
+              <MenuItem value='youtube'>Youtube</MenuItem>
+              <MenuItem value='twitter'>Twitter</MenuItem>
+            </TextField>
+            <InputElement
+              label='Tag'
+              type='text'
+              value={item.tag}
+              onInput={inputText => {
+                const network = socials[index]?.network ?? 'none'
+                addSocials(index, {network, tag: inputText ?? ''})
+              }}
+            />
+            <ButtonWrapper
+              addItemFunction={() => {
+                addSocials((socials.length + 1), {network: 'none'})
+              }}
+              removeItemFunction={removeSocials}
+              index={index}
+              template={item}
+              hideRemove={socials.length === 1}
+              hideAdd={socials.length === 4}
+            />
+          </SocialsToolbar>)
+        }
+      </SocialsWrapper>
+    </ControllersWrapper>
+  }
 
 export default HeaderDesigner
