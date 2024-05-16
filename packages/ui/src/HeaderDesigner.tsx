d584df49418a57f539ff3717ec8de8cab54f8d24
ohadn <ohad111@gmail.com>
2024-05-16 13:29:43 +0200

    Add CollapsibleSection component to UI package
    Implemented a CollapsibleSection component to improve the organization of the page layout. This new component was added to the HeaderDesigner, thus, making the content clearer and more manageable. Updated the package.json file to include the new component.

--- packages/ui/src/HeaderDesigner.tsx
+++ packages/ui/src/HeaderDesigner.tsx
@@ -7,13 +7,17 @@ import {SocialNetworks, Socials} from "web/state/schedule";
 import {Button, MenuItem, Select, TextField} from "@mui/material";
 import styled from "@emotion/styled";
 import ButtonWrapper from "./ButtonWrapper";
+import CollapsibleSection from "./collapsibleSection";
 
 
 const ControllersWrapper = styled.section`
-  padding: 16px;
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    padding: 16px;
 `
 const SocialsWrapper = styled.div`
-  margin-top: 32px;
+
 `
 const SocialsToolbar = styled.div`
     width: 100%;
@@ -41,52 +45,57 @@ const HeaderDesigner: React.FC<HeaderDesignerProps> =
    }) => {
 
     return <ControllersWrapper {...props}>
-      <InputElement
-        label='Total days:'
-        type='text'
-        value={mainHeader}
-        onInput={inputText =>
-          setMainHeader(inputText ?? '')}
-      />
-      <SocialsWrapper>
-        {
-          socials.map((item, index) => <SocialsToolbar key={item.network + index}>
-            <TextField
-              select
-              label='Select Network'
-              value={socials[index]?.network}
-              onChange={(event) => {
-                addSocials((index), {...socials[index], network: event.target.value as SocialNetworks})
-              }}
-              style={{flexBasis: '200px'}}
-            >
-              <MenuItem value='none'>None</MenuItem>
-              <MenuItem value='twitch'>Twitch</MenuItem>
-              <MenuItem value='youtube'>Youtube</MenuItem>
-              <MenuItem value='twitter'>Twitter</MenuItem>
-            </TextField>
-            <InputElement
-              label='Tag'
-              type='text'
-              value={item.tag}
-              onInput={inputText => {
-                const network = socials[index]?.network ?? 'none'
-                addSocials(index, {network, tag: inputText ?? ''})
-              }}
-            />
-            <ButtonWrapper
-              addItemFunction={() => {
-                addSocials((socials.length + 1), {network: 'none'})
-              }}
-              removeItemFunction={removeSocials}
-              index={index}
-              template={item}
-              hideRemove={socials.length === 1}
-              hideAdd={socials.length === 4}
-            />
-          </SocialsToolbar>)
-        }
-      </SocialsWrapper>
+      <CollapsibleSection title='Header Text'>
+        <InputElement
+          label='Total days:'
+          type='text'
+          value={mainHeader}
+          onInput={inputText =>
+            setMainHeader(inputText ?? '')}
+        />
+      </CollapsibleSection>
+      <CollapsibleSection title='Socials'>
+        <SocialsWrapper>
+          {
+            socials.map((item, index) => <SocialsToolbar key={item.network + index}>
+              <TextField
+                select
+                label='Select Network'
+                value={socials[index]?.network}
+                onChange={(event) => {
+                  addSocials((index), {...socials[index], network: event.target.value as SocialNetworks})
+                }}
+                style={{flexBasis: '200px'}}
+              >
+                <MenuItem value='none'>None</MenuItem>
+                <MenuItem value='twitch'>Twitch</MenuItem>
+                <MenuItem value='youtube'>Youtube</MenuItem>
+                <MenuItem value='twitter'>Twitter</MenuItem>
+              </TextField>
+              <InputElement
+                label='Tag'
+                type='text'
+                value={item.tag}
+                onInput={inputText => {
+                  const network = socials[index]?.network ?? 'none'
+                  addSocials(index, {network, tag: inputText ?? ''})
+                }}
+              />
+              <ButtonWrapper
+                addItemFunction={() => {
+                  addSocials((socials.length + 1), {network: 'none'})
+                }}
+                removeItemFunction={removeSocials}
+                index={index}
+                template={item}
+                hideRemove={socials.length === 1}
+                hideAdd={socials.length === 4}
+              />
+            </SocialsToolbar>)
+          }
+        </SocialsWrapper>
+      </CollapsibleSection>
+
     </ControllersWrapper>
   }
 
---
+++ packages/ui/src/collapsibleSection.tsx
@@ -0,0 +1,67 @@
+import * as React from "react";
+import styled from "@emotion/styled";
+import {css} from "@emotion/react";
+import {useState} from "react";
+
+const CollapsibleSectionWrapper = styled.section`
+    border-bottom: 1px solid #e8e8e8;
+    padding-bottom: 16px;
+    border-radius: 4px;
+    & > :nth-child(2) {
+        margin-top: 16px;
+    }
+`
+const SectionTopBar = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    cursor: pointer;
+    padding: 8px;
+    border-radius: 4px;
+    background: #f8f8f8;
+`
+
+const IconWrapper = styled.svg<{ $invert: boolean }>`
+    height: 24px;
+    transition: transform 200ms ease-in-out;
+    ${props => props.$invert && css`transform: rotate(180deg);`}
+`
+type ChevronDownProps = {
+  invert?: boolean
+} & React.ComponentPropsWithoutRef<'svg'>
+
+const ChevronDown: React.FC<ChevronDownProps> = ({invert, ...props}) => {
+  return <IconWrapper
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    $invert={invert ?? true}
+    {...props}
+  >
+    <path fillRule="evenodd" clipRule="evenodd"
+          d="M4.29289 8.29289C4.68342 7.90237 5.31658 7.90237 5.70711 8.29289L12 14.5858L18.2929 8.29289C18.6834 7.90237 19.3166 7.90237 19.7071 8.29289C20.0976 8.68342 20.0976 9.31658 19.7071 9.70711L12.7071 16.7071C12.3166 17.0976 11.6834 17.0976 11.2929 16.7071L4.29289 9.70711C3.90237 9.31658 3.90237 8.68342 4.29289 8.29289Z"
+          fill="#000000"/>
+  </IconWrapper>
+}
+
+type CollapsibleSectionProps = {
+  title: string
+  children: React.ReactNode;
+} & React.ComponentPropsWithoutRef<'section'>
+
+const CollapsibleSection: React.FC<CollapsibleSectionProps> = ({title, children, ...props}) => {
+  const [isExtended, setIsExtended] = useState(true)
+
+  return <CollapsibleSectionWrapper {...props}>
+    <SectionTopBar onClick={() => setIsExtended(!isExtended)}>
+      <span>{title}</span>
+
+      <ChevronDown
+        invert={isExtended}
+      />
+    </SectionTopBar>
+    {isExtended && children}
+  </CollapsibleSectionWrapper>
+}
+
+export default CollapsibleSection
