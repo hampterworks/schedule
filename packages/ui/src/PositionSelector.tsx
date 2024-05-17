84ff8909bd2d86e1860fdf105f7947a640803b0e
ohadn <ohad111@gmail.com>
2024-05-17 03:20:16 +0200

    Refactor PositionSelector component logic
    The PositionSelector component logic has been refactored. This included switching string-based use of coordinates in the Selector styled-component to directly passing x and y as strings. For background positioning calculation, useMemo is now being used. Also, transformations were applied when setting and reading the background position to adjust the coordinate system, and the mousemove event listener was updated with the passive option for better performance.

--- packages/ui/src/PositionSelector.tsx
+++ packages/ui/src/PositionSelector.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import styled from "@emotion/styled";
-import {useCallback, useEffect, useState} from "react";
+import {useCallback, useEffect, useMemo, useState} from "react";
 import {BackgroundPosition, BackgroundSize, Color} from "web/state/schedule";
 
 const PositionSelectorWrapper = styled.div`
@@ -10,11 +10,11 @@ const PositionSelectorWrapper = styled.div`
     border: 1px solid black;
 `
 
-const Selector = styled.div<{ x: number; y: number }>`
+const Selector = styled.div<{ x: string; y: string }>`
     position: absolute;
     cursor: pointer;
-    top: ${(props) => props.y.toString() + '%'};
-    left: ${(props) => props.x.toString() + '%'};
+    top: ${(props) => props.y};
+    left: ${(props) => props.x};
     height: 15px;
     width: 15px;
     border: 3px solid black;
@@ -28,13 +28,14 @@ type PositionSelectorProps = {
 } & React.ComponentPropsWithoutRef<'div'>
 
 const PositionSelector: React.FC<PositionSelectorProps> = ({backgroundPosition, setBackgroundPosition, ...props}) => {
-  const transformPosition =
-    backgroundPosition === 'center'
+  const transformPosition = useMemo(() => {
+    return backgroundPosition === 'center'
       ? {x: 50, y: 50}
       : {
-        x: parseInt(backgroundPosition.split(' ')[0]?.replace('%', '') ?? '50'),
-        y: parseInt(backgroundPosition.split(' ')[1]?.replace('%', '') ?? '50')
+        x: ((parseInt(backgroundPosition.split(' ')[0]?.replace('%', '') ?? '50') + 100) / 300) * 100,
+        y: ((parseInt(backgroundPosition.split(' ')[1]?.replace('%', '') ?? '50') + 100) / 300) * 100
       }
+  }, [backgroundPosition])
 
   const [position, setPosition] = useState(transformPosition)
   const [dragging, setDragging] = useState(false)
@@ -63,7 +64,7 @@ const PositionSelector: React.FC<PositionSelectorProps> = ({backgroundPosition, 
 
   useEffect(() => {
     if (ref.current) {
-      ref.current.addEventListener("mousemove", handleMouseMove)
+      ref.current.addEventListener("mousemove", handleMouseMove, {passive: true})
     }
     return () => {
       if (ref.current) {
@@ -73,7 +74,7 @@ const PositionSelector: React.FC<PositionSelectorProps> = ({backgroundPosition, 
   }, [handleMouseMove])
 
   useEffect(() => {
-    setBackgroundPosition(`${position.x.toString() + '%'} ${position.y.toString() + '%'}`)
+    setBackgroundPosition(`${((position.x / 100) * 300 - 100).toString() + '%'} ${((position.y / 100) * 300 - 100).toString() + '%'}`)
   }, [position])
 
   return <PositionSelectorWrapper
@@ -83,7 +84,7 @@ const PositionSelector: React.FC<PositionSelectorProps> = ({backgroundPosition, 
     onMouseLeave={handleMouseUp}
     {...props}
   >
-    <Selector x={position.x} y={position.y}/>
+    <Selector x={`${position.x}%`} y={`${position.y}%`}/>
   </PositionSelectorWrapper>
 
 }
