fd71d7955a9b5e60b80eaf4b03c5e179df284245
ohadn <ohad111@gmail.com>
2024-05-26 16:00:00 +0200

    Add Google site verification and OpenGraph meta tag
    The changes introduced add Google site verification and OpenGraph image meta tag to the head section of the root layout. This is essential for SEO and social media sharing as it helps Google verify the site, and provides an image for OpenGraph to use when the site's URL is shared.

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -5,19 +5,21 @@ import HydrationZustand from "./HydrationZustand";
 import React from "react";
 import ApplicationFrame from "@repo/ui/ApplicationFrame";
 import Header from "@repo/ui/Header";
+import Head from "next/head";
 
 const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
   title: "Hampter Schedule",
   description: "Generate streaming schedules with time zone and discord support",
-  openGraph: {
-    images: '/schedule/hampter.png'
-  }
-};
+}
 
 const RootLayout: React.FC<{ children: React.ReactNode }> = ({children}) => {
   return <html lang="en">
+  <Head>
+    <meta name="google-site-verification" content="lgaawbBW07t8QCZK3-U3IwFHKf34IuCV-8p5bvUGjGA"/>
+    <meta property="og:image" content='/schedule/hampter.png'/>
+  </Head>
   <body className={inter.className}>
   <HydrationZustand>
     <ApplicationFrame>
