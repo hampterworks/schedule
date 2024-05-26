f841d14eb5fe0db3628af7259e8281049c9d895a
ohadn <ohad111@gmail.com>
2024-05-26 16:09:36 +0200

    Add metadata and remove redundant tags in layout component
    This commit extends the 'metadata' object, and subsequently removes duplicate meta information from the head tag in the layout.tsx component file. The changes include the addition of a base URL, openGraph, keywords, and verification properties to the metadata.

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -5,21 +5,32 @@ import HydrationZustand from "./HydrationZustand";
 import React from "react";
 import ApplicationFrame from "@repo/ui/ApplicationFrame";
 import Header from "@repo/ui/Header";
-import Head from "next/head";
 
 const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://hampterworks.github.io/schedule/'),
   title: "Hampter Schedule",
   description: "Generate streaming schedules with time zone and discord support",
+  openGraph: {
+    images: '/hampter.png'
+  },
+  keywords:[
+    'schedule',
+    'streaming schedule',
+    'vtuber schedule',
+    'schedule generator',
+    'streaming',
+    'twitch schedule',
+    'youtube schedule'
+  ],
+  verification:{
+    google: "lgaawbBW07t8QCZK3-U3IwFHKf34IuCV-8p5bvUGjGA"
+  }
 }
 
 const RootLayout: React.FC<{ children: React.ReactNode }> = ({children}) => {
   return <html lang="en">
-  <Head>
-    <meta name="google-site-verification" content="lgaawbBW07t8QCZK3-U3IwFHKf34IuCV-8p5bvUGjGA"/>
-    <meta property="og:image" content='/schedule/hampter.png'/>
-  </Head>
   <body className={inter.className}>
   <HydrationZustand>
     <ApplicationFrame>
