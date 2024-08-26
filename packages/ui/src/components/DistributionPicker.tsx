e8f10d03367777a1abe54b2dbb7964881689ebde
ohadn <ohad111@gmail.com>
2024-08-26 19:05:51 +0200

    Update DistributionPicker title styling with styled-components
    Replaced the inline title div with a styled TitleContainer for 'Select Distribution' using styled-components. This change improves readability and ensures consistent styling for the picker component.

--- packages/ui/src/components/DistributionPicker.tsx
+++ packages/ui/src/components/DistributionPicker.tsx
@@ -3,6 +3,14 @@
 import * as React from "react";
 import Distribution from "../icons/Distribution";
 import {Distribution as DataDistribution, DistributionFn} from "web/state/schedule";
+import styled from "styled-components";
+
+const TitleContainer = styled.div`
+    margin: 0 0 4px 4px;
+    color: rgba(0, 0, 0, 0.6);
+    font-size: 13px;
+    white-space: nowrap;
+`
 
 type DistributionPickerProps = {
   dateDistribution: DataDistribution
@@ -13,7 +21,7 @@ type DistributionPickerProps = {
 const DistributionPicker: React.FC<DistributionPickerProps> = ({dateDistribution, setDistributionAlignment, ...props}) => {
 
   return <div {...props}>
-    <div>Select Alignment</div>
+    <TitleContainer>Select Distribution</TitleContainer>
     <Distribution
       toggle={dateDistribution === 'list'}
       onSelected={(event) => {
